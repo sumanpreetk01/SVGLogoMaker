09fb6a71b5ca5a0c37a77dfbcb2b1f935076a6a0
Sumanpreet Kaur <sharankaur@sharans-air.lan>
2023-06-24 09:15:41 -0400

    added test

--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,38 @@
-const {Circle} = require("../lib/shapes")
+const {Triangle,Circle,Square} = require("../lib/shapes")
 
-test('Circle is class with 3 properties', () => {
-   const example = new Circle()
-   expect(Object.keys(example).length).toBe(3)
-});
-
-test('Circle can be assigned property text as first argument', () => {
-    const example = new Circle("AAA")
-    expect(example.text).toBe("AAA")
+ test('Triangle shape colour is blue', () => {
+    const shape = new Triangle();
+    shape.shapeColour = "blue";
+    expect(shape.render()).toBe('<polygon points="150,10 300,190 20,190" style="fill:blue" />');
  });
 
- test('Circle can be assigned property shapeColour as last and third argument', () => {
-    const example = new Circle("AAA", "Blue", "Green", "WHite")
-    expect(example.shapeColour).toBe("Green")
+ test('Circle shape colour is green', () => {
+    const shape = new Circle();
+    shape.shapeColour = "green";
+    expect(shape.render()).toBe('<circle cx="150" cy="100" r="80" fill="green" />');
  });
+
+ test('Square shape colour is red', () => {
+    const shape = new Square();
+    shape.shapeColour = "red";
+    expect(shape.render()).toBe('<rect x="60" y="20" width="180" height="180" style="fill:red" />');
+ });
+ 
+
+ 
+ //Notes *disregard*
+ 
+ // test('Circle is class with 3 properties', () => {
+//    const example = new Circle()
+//    expect(Object.keys(example).length).toBe(3)
+// });
+
+// test('Circle can be assigned property text as first argument', () => {
+//     const example = new Circle("AAA")
+//     expect(example.text).toBe("AAA")
+//  });
+
+//  test('Circle can be assigned property shapeColour as last and third argument', () => {
+//     const example = new Circle("AAA", "Blue", "Green", "WHite")
+//     expect(example.shapeColour).toBe("Green")
+//  });
